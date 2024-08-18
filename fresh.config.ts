6f6571d5da7b489b3fd523f51ce6758a42d83e8d
tunatuna1733 <bouningen13@gmail.com>
2024-08-18 15:42:00 +0900

    added docker related things

--- fresh.config.ts
+++ fresh.config.ts
@@ -2,5 +2,8 @@ import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
 
 export default defineConfig({
+  server: {
+    port: 7000,
+  },
   plugins: [tailwind()],
 });
